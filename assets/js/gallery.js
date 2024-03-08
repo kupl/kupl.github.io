a895eab79e6e4bb0cbae592489108067ec6f48a7
Henry Lee <gbvrcx@gmail.com>
2024-03-08 05:01:03 +0000

    :sparkles: Nicer gallery view

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,11 +1,40 @@
-const galleries = document.querySelectorAll('.gallery');
-galleries.forEach((gallery) => {
-    const masonry = new Masonry(gallery, {
-        itemSelector: '.item',
-        percentPosition: true
+// Enable lazy loading for image thumbnails
+document.addEventListener("DOMContentLoaded", function () {
+    const lazyloadImages = document.querySelectorAll(".lazy");
+    const imageObserver = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(function (entry) {
+            if (entry.isIntersecting) {
+                const image = entry.target;
+                image.src = image.dataset.src;
+                image.classList.remove("lazy");
+                imageObserver.unobserve(image);
+            }
+        });
     });
 
-    imagesLoaded(gallery).on('progress', function () {
-        masonry.layout();
+    lazyloadImages.forEach(function (image) {
+        imageObserver.observe(image);
     });
-}) 
+
+});
+
+// Build masonry layout one by one
+const galleryHolders = document.querySelectorAll('.gallery-holder');
+galleryHolders.forEach((galleryHolder) => {
+    const target = document.querySelector(galleryHolder.dataset.galleryTarget);
+    const masonry = new Masonry(target, {
+        itemSelector: '.gallery-item',
+        percentPosition: true,
+        transitionDuration: 0,
+    });
+
+    const nItems = galleryHolder.children.length;
+    for (let i = 0; i < nItems; i++) {
+        const item = galleryHolder.children[0];
+        target.appendChild(item);
+        masonry.appended([item]);
+        masonry.layout();
+    }
+
+    galleryHolder.remove();
+});
